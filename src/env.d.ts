5f2543c7d847a63eb0fb0d0558c5019658420a30
shafucheng <shafucheng@outlook.com>
2022-08-30 18:06:21 +0800

    chore: vite define __APP_VERSION__

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+
+// vite define
+declare const __APP_VERSION__: string
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+import fs from 'fs'
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const packageJson = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, 'package.json'), 'utf-8'),
+)
+
 export default defineConfig({
+  define: {
+    __APP_VERSION__: `"${packageJson.version}"`,
+  },
   plugins: [react()],
   resolve: {
     alias: {
