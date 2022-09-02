aba939d2fa578651bb4aa873951851ecdbbfeb60
shafucheng <shafucheng@outlook.com>
2022-09-02 14:00:35 +0800

    style: eslint --fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import fs from 'fs'
 import path from 'path'
-import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 const packageJson = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, 'package.json'), 'utf-8'),
