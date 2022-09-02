a9a541b97765e90bed694734f86a21589e263130
shafucheng <shafucheng@outlook.com>
2022-09-02 15:10:02 +0800

    feat: add antd ConfigProvider locale=zhCN

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
+import 'dayjs/locale/zh-cn'
+
 const App = () => {
-  return <div>Hello World!</div>
+  return (
+    <ConfigProvider locale={zhCN}>
+      <div>Hello World!</div>
+    </ConfigProvider>
+  )
 }
 
 export default App
