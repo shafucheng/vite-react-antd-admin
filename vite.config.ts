05b8d36da9716dc8391231e402689eefe69aa581
shafucheng <shafucheng@outlook.com>
2022-09-02 14:30:17 +0800

    chore: auto import antd style

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import momentToDayjs from 'unplugin-moment-to-dayjs/vite'
 import { defineConfig } from 'vite'
+import imp from 'vite-plugin-imp'
 
 const packageJson = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, 'package.json'), 'utf-8'),
@@ -13,7 +14,19 @@ export default defineConfig({
   define: {
     __APP_VERSION__: `"${packageJson.version}"`,
   },
-  plugins: [react(), momentToDayjs()],
+  plugins: [
+    react(),
+    momentToDayjs(),
+    imp({
+      libList: [
+        {
+          libName: 'antd',
+          libDirectory: 'es',
+          style: (name) => `antd/es/${name}/style`,
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
